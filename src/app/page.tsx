0ccb7c1151202ab70d7facd00c5286d11675770f
UmairRehman1024 <85501421+UmairRehman1024@users.noreply.github.com>
2024-07-08 18:21:45 +0100

    put queries into seperate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
@@ -16,9 +17,7 @@ const mockImages = mockURLs.map((url, index) => ({
   url,
 }));
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
   return (
     <div className="flex flex-wrap gap-4 p-8">
       {images.map((image) => (
