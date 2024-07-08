b75f05817fbe905510956f3140faa379d456c8d9
UmairRehman1024 <85501421+UmairRehman1024@users.noreply.github.com>
2024-07-08 23:53:28 +0100

    removed unnecesary mocking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,6 @@ import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
-const mockURLs = [
-  "https://utfs.io/f/878d8c27-cc75-4aeb-8de2-7a2bfdbda5af-1nqriq.png",
-  "https://utfs.io/f/44d9f548-a0c8-480c-936c-c948f07cd4df-13f8ci.jpg",
-  "https://utfs.io/f/1ab14a50-f59f-442d-9e81-cfe32dabf3bf-x4qg1h.png",
-  "https://utfs.io/f/e164afbc-9898-4470-afb9-15eefeff6e6c-xqvczi.png",
-];
-
-const mockImages = mockURLs.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 async function Images() {
   const images = await getMyImages();
   return (
