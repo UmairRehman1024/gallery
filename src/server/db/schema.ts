dbce8216657e4af11d770c675df74e5d48b9c364
UmairRehman1024 <85501421+UmairRehman1024@users.noreply.github.com>
2024-07-11 22:33:32 +0100

    add albums to database schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,7 +3,9 @@
 
 import { sql } from "drizzle-orm";
 import {
+  AnyPgColumn,
   index,
+  integer,
   pgTableCreator,
   serial,
   timestamp,
@@ -18,25 +20,30 @@ import {
  */
 export const createTable = pgTableCreator((name) => `gallery_${name}`);
 
-export const images = createTable(
-  "image",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }).notNull(),
-    url: varchar("url", { length: 1024 }).notNull(),
+export const albums = createTable("albums", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  userId: varchar("userId", { length: 256 }).notNull(),
+  parentId: integer("parent_id").references((): AnyPgColumn => albums.id), // Self-referencing foreign key for nested folders
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+    () => new Date(),
+  ),
+});
 
-    userId: varchar("userId", { length: 256 }).notNull(),
-
-    key: varchar("key", { length: 256 }).notNull(),
-
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date(),
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  }),
-);
+export const images = createTable("images", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  url: varchar("url", { length: 1024 }).notNull(),
+  userId: varchar("userId", { length: 256 }).notNull(),
+  key: varchar("key", { length: 256 }).notNull(),
+  albumId: integer("albumId").references(() => albums.id), // Foreign key to albums table
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+    () => new Date(),
+  ),
+});
